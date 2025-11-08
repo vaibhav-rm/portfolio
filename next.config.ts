351376ae4e66d2692573939212902fc5c941ba57
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-11-08 15:01:11 +0530

    Fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 👈 tells Next.js to produce a static /out folder
+  images: { unoptimized: true }, // 👈 needed for static export
 };
 
-export default nextConfig;
+module.exports = nextConfig;
